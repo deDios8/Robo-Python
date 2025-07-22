82a11c2a027d12ce3b9936d1d855ef40e0bdee58
DarianTyan <tyandarian@gmail.com>
2025-07-22 10:07:16 -0500

    Second trial for two buttons at once

--- robot.py
+++ robot.py
@@ -23,15 +23,18 @@ class RobotContainer:
         self.ss_general_motor = SS_GeneralMotor()
         self.ss_general_servo = SS_GeneralServo()
 
-    
+
 
     def keybindings(self):
         #when press and hold A, it goes to max position. when released, it goes back to min position. When button is pressed and held and b is pressed, it goes halfway.
         self.controller.x().onTrue(self.ss_general_motor.run_forward_command())
         self.controller.x().onFalse(self.ss_general_motor.stop_motor_command())
         self.controller.a().onTrue(self.ss_general_servo.run_to_max_position_command())
-        self.controller.a().onTrue and self.controller.b().onTrue(self.ss_general_servo.run_to_A_position_command())
         self.controller.a().onFalse(self.ss_general_servo.run_to_min_position_command())
+        
+        if self.controller.a() and self.controller.b():
+            self.controller.a().onTrue(self.ss_general_servo.run_to_A_position_command())
+            self.controller.b().onTrue(self.ss_general_servo.run_to_A_position_command())
 
         self.controller.y().onFalse(self.ss_general_servo.run_to_A_position_command())
         self.controller.rightBumper().whileTrue(self.ss_general_servo.adjust_servo_ahead_command())
